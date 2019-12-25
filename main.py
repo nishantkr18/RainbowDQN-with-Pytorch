c0f4647ec6b43af117bb688f28cbe77e4a0af6b0
nishantkr18 <nishantkr.ece18@itbhu.ac.in>
2019-12-25 13:32:41 +0530

    added comments to main.py

--- main.py
+++ main.py
@@ -15,13 +15,26 @@ target_update = 100
 epsilon_decay = 1 / 2000
 obs_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
+
+#----------------------------------------------------
+# Double DQN use can be enabled using this
 double_dqn = True
 
+# for experience replay to be randomly sampled
 replay_method = RandomReplay(obs_dim, memory_size, batch_size)
+
+# Using the simplest DQN
 network = VanillaDQN(obs_dim, 128, action_dim)
+
+# Using Dueling DQN
 network = DuelingDQN(obs_dim, 128, action_dim)
+#-----------------------------------------------------
+
+# Initilizing the agent
 agent = DQNAgent(env, network, replay_method, memory_size, batch_size, target_update, epsilon_decay, double_dqn)
 
+# Training the agent
 agent.train(num_frames)
 
+# Tests the agent
 agent.test()
